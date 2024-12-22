f16bc892cfce16398fef14bd9edac3346394e39a
weeduck12 <bouna135@hotmail.fr>
2024-12-22 16:36:27 +0100

    ADVANCEMENT SDL PAGE FONCTIONNELLE

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
 
 int main()
 {
-	printf("hello world\n");
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("ERREUR SDL : %s\n",SDL_GetError());
+		return 1;
+	}
+	printf("Initialisation réalisée avec succès\n");
+	SDL_Window* fenetre = NULL;
+	fenetre = SDL_CreateWindow("Fenetre GAMECRASH",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,640,480,SDL_WINDOW_SHOWN);
+
+	if(fenetre)
+	{
+		SDL_Delay(3000);
+		SDL_DestroyWindow(fenetre);
+	}
+	else
+	{
+		fprintf(stderr,"Erreur de creation de la fenetre : %s\n",SDL_GetError());
+	}
+	SDL_Quit();
 	return 0;
 }
