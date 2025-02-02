236f7658696da4290c62f34805be07ffde88ac0c
weeduck12 <bouna135@hotmail.fr>
2025-02-02 14:28:36 +0100

    ajout pion visuel

--- main.c
+++ main.c
@@ -25,11 +25,13 @@ int main()
 	SDL_Surface* surface = IMG_Load("plateau.png");
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(rendu, surface);
 	SDL_Surface* pions = IMG_Load("pions.png");
-        SDL_Texture* texture = SDL_CreateTextureFromSurface(rendu, pions);
+        SDL_Texture* texturepions = SDL_CreateTextureFromSurface(rendu, pions);
 	SDL_FreeSurface(surface);
 	SDL_FreeSurface(pions);
 //DECLARATIONS 
 	SDL_Event event;
+	SDL_Rect pion = {1500, 450, 200, 300};
+	SDL_Rect place_pion = {10, 750,200 ,300};
 //LOOP
 	bool run = true;
 	while(run){
@@ -43,11 +45,13 @@ int main()
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
         SDL_RenderClear(rendu);
         SDL_RenderCopy(rendu, texture, NULL, NULL);
+	SDL_RenderCopy(rendu, texturepions, &pion, &place_pion);
         SDL_RenderPresent(rendu);
 	//
 	}
 	//FERMETURE
 	SDL_DestroyTexture(texture);
+	SDL_DestroyTexture(texturepions);
 	SDL_DestroyRenderer(rendu);
 	SDL_DestroyWindow(fenetre);
 	SDL_Quit();
